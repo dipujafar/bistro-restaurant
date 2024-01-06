368d5bfa8436af43f03aef9bc38337a8360bdcd5
dipujafar <dipujafarmr@gmail.com>
2024-01-06 21:09:41 +0600

    add admin home all stats

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -4,7 +4,7 @@ import useAuth from './useAuth';
 
 
 const axiosSecure = axios.create({
-    baseURL: 'http://localhost:5000'
+    baseURL: 'https://bistro-restaurant-server-gamma.vercel.app'
 })
 
 const useAxiosSecure = () => {
